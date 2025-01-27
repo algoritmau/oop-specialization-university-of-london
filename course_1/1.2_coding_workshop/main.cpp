c916f85b6a0377e725f3fe182833991337503ab9
algoritmau <49292621+algoritmau@users.noreply.github.com>
2025-01-27 18:26:04 -0500

    print menu options

---
+++ course_1/1.2_coding_workshop/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Print a list of options (menu)
+    cout << "1: Print help." << endl;
+    cout << "2: Print exchange stats." << endl;
+    cout << "3: Make an offer." << endl;
+    cout << "4: Make a bid." << endl;
+    cout << "5: Print wallet." << endl;
+    cout << "6: Continue" << endl;
+
+    return 0;
+}
